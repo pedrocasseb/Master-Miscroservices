2ba45616d45f4064cb5f40659c60a53ea88ce07a
pedrocasseb <casseb.phcc@gmail.com>
2026-08-08 14:33:28 -0300

    feat(loans): implements loans REST API

---
+++ loans/src/main/java/com/microservices/loans/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.microservices.loans.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(updatable = false)
+    @CreatedBy
+    private String createdBy;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    private String updatedBy;
+}
